1726a66e0d1a6f92b5a68262f4152692872146f4
erica223020 <92545899+erica223020@users.noreply.github.com>
2022-05-13 06:16:19 +0800

    新增下拉式選單功能

--- js/all.js
+++ js/all.js
@@ -5,20 +5,47 @@ let dataObject = {} ;
 //建立新的連線
 let requestURL = new XMLHttpRequest();
 
-requestURL.open('get','https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c');
+requestURL.open('get','https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json');
 requestURL.send();
 
-//頁面完成加載時觸發reqListener
-requestURL.addEventListener('load',reqListener);
+//頁面加載完成後立刻執行以下
+requestURL.onload = 
 
-
+//確認是否讀取到資料，並分析json內的資料
 function reqListener () {
     if(requestURL.status == 200){
         let opendata = JSON.parse(requestURL.responseText);
-        dataObject = opendata.data.XML_Head.Infos.Info; //XML_Head.Infos.Info是讀取節點資訊
+        dataObject = opendata.result.records; //result.records是讀取資料所回傳的路徑
+
+//下拉式選單，將Zone的區域抓出來
+        let catchArea = [];
+        let len = dataObject.length;
+        for(let i=0;len>i;i++){
+        catchArea.push(dataObject[i].Zone);
+        }
+
+//使用Set的特性篩選重複的區域
+        let dataSet = new Set(catchArea);
+        catchArea = [...dataSet];//用...來組合(連接)陣列，後面必定接著一個陣列
+
+//將篩選完的陣列資料放入下拉式選單
+        let areaLen = catchArea.length;
+        let chooseArea = document.querySelector('.chooseArea');
+        for (let i=0;areaLen>i;i++){
+            let newOption = document.createElement('option');
+            newOption.textContent = catchArea[i];
+            newOption.value = catchArea[i];
+            chooseArea.appendChild(newOption);
+              
+        }
     }
+
+      
+    
     else{
         alert("資料讀取錯誤");
     }
 }
+
+
 
